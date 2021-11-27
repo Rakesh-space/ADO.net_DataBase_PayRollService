22f35f1972d257656e26e8be671bc588dbabe7e2
unknown <rakeshsmusale03@gmail.com>
2021-11-27 01:16:09 -0800

    Welcome message.

---
+++ ADO.net_DataBase_PayRollService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.net_DataBase_PayRollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to ADO.net Pay Roll Service..");
+        }
+    }
+}
